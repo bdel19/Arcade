c51faacd7322fa2f83df6ddd99b7c455c9ec2e83
Brandon <bmd21991@yahoo.com>
2022-09-17 17:38:52 -0500

    Progress update

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
-// state
-let initialState;
+// ******************** state ********************
+let state;
+const boardElem = document.getElementById('board');
+const playerTurnElem = document.querySelector("#player-turn");
+console.log(playerTurnElem);
+
+
 
 function buildInitialState() {
-
+  state = {
+    players: ['x', 'o'],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
+  }
+  
+  state.currentPlayer = ["Jack","Jill"]
+  state.currentPlayerIdx = 0;
+  
 }
 
-// render
+
+
+//******************** render ********************
 function renderState() {
-
+  for(let i=0; i<state.board.length; i++){
+    let something = state.board[i];
+      for (let j = 0; j < something.length; j++){
+        const cell = document.createElement("div");
+        board.appendChild(cell);
+        cell.innerText="Cell"
+      }
+    }
 }
+
+
 
 // maybe a dozen or so helper functions for tiny pieces of the interface
 
-// listeners
+
+// ******************** logic ********************
+function changeTurn(){
+  if (state.currentPlayerIdx === 0) {
+    state.currentPlayerIdx = 1;
+  } else {
+    state.currentPlayerIdx = 0;
+    }
+}
+
+//********************listeners********************
+
 function onBoardClick() {
   // update state, maybe with another dozen or so helper functions...
 
   renderState() // show the user the new state
 }
-const board = document.getElementById('board');
-board.addEventListener('click', onBoardClick); // etc
 
-// add to above
-function tick() {
-    // this is an incremental change that happens to the state every time you update...
-  
-    renderState()
-  }
-  
-  setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
-  
-  // now you might have things like
-  document.addEventListener('keydown', function (event) {
-    // here you might read which key was pressed and update the state accordingly
-  })
+// boardElem.addEventListener('click', onBoardClick); // etc
 
-  let gameState = {
-    apple: [11, 8],
-    snake: snake // from above
-  }
-
-  let snake = {
-    body: [ [10, 5], [10, 6], [10, 7], [10, 8] ],
-    nextDirection: [1, 0]
-  }
+buildInitialState()
+renderState()
+changeTurn()
+console.log(state.currentPlayer[state.currentPlayerIdx]);
