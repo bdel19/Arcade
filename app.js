2c49192e1124e8233091c58e6a9227c5bb76a993
Brandon <bmd21991@yahoo.com>
2022-09-17 20:57:13 -0500

    added game logic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let state;
 const boardElem = document.getElementById('board');
 const playerTurnElem = document.querySelector("#player-turn");
-console.log(playerTurnElem);
+
 
 
 
@@ -16,15 +16,18 @@ function buildInitialState() {
     ]
   }
   
-  state.currentPlayer = ["Jack","Jill"]
+  state.currentPlayer = ["",""]
   state.currentPlayerIdx = 0;
-  
+  state.getCurrentPlayer = function (){
+    return state.currentPlayer[state.currentPlayerIdx];
+  };
 }
 
 
 
 //******************** render ********************
 function renderState() {
+  boardElem.innerHTML = ""
   for(let i=0; i<state.board.length; i++){
     let something = state.board[i];
       for (let j = 0; j < something.length; j++){
@@ -47,19 +50,47 @@ function changeTurn(){
   } else {
     state.currentPlayerIdx = 0;
     }
+    console.log(state.getCurrentPlayer());
+}
+
+function renderPlayer() {
+  let text;
+  if (!state.currentPlayer[0] || !state.currentPlayer[1]){
+    text = `
+    <input name="player1" placeholder="Enter Player 1 Name">
+    <input name="player2" placeholder="Enter Player 2 Name">
+    <button class="start"> Start Game </button>
+    `;
+  }else {
+    text = `It's currently <span class="player">${state.getCurrentPlayer()}</span>'s turn`
+  }
+  playerTurnElem.innerHTML = text;
 }
 
 //********************listeners********************
-
+boardElem.addEventListener('click', onBoardClick);
 function onBoardClick() {
+  changeTurn();
+  renderPlayer();
   // update state, maybe with another dozen or so helper functions...
-
-  renderState() // show the user the new state
+renderState();
+  // renderState() // show the user the new state
 }
-
+playerTurnElem.addEventListener("click", function(event){
+  console.log("click");
+  if (event.target.className !== "start"){
+    return;
+  }
+  const player1Input = document.querySelector("input[name=player1");
+  const player2Input = document.querySelector("input[name=player2");
+  console.log("player1Input", player1Input.value);
+  console.log("player2Input", player2Input.value);
+  state.currentPlayer[0] = player1Input.value;
+  state.currentPlayer[1] = player2Input.value;
+  renderPlayer();
+});
 // boardElem.addEventListener('click', onBoardClick); // etc
 
-buildInitialState()
-renderState()
-changeTurn()
-console.log(state.currentPlayer[state.currentPlayerIdx]);
+buildInitialState();
+renderState();
+renderPlayer();
